b16bf32ad17054fe58fe60ae8e53a7dcb5465822
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2025-04-03 17:09:31 +0200

    Enhance chatbot functionality with structured output and summary generation; add memory management and conditional workflow handling

--- chatbot.py
+++ chatbot.py
@@ -2,8 +2,11 @@
 #! Imports
 from langchain_chroma import Chroma
 
+from langchain_community.chat_message_histories import FileChatMessageHistory
+
 from langchain_mistralai import ChatMistralAI
 from langchain_mistralai import MistralAIEmbeddings
+from langchain.chat_models import init_chat_model
 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.documents import Document
@@ -11,20 +14,35 @@ from langchain_core.runnables import chain
 from langchain_core.messages import BaseMessage
 
 from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, END, MessagesState
 
-from langchain.memory import ConversationSummaryMemory
+from langgraph.checkpoint.memory import MemorySaver
 
 from typing import List, Sequence
 from typing_extensions import Annotated, TypedDict
+
+from pydantic import BaseModel, Field
 
 
 import dotenv
 import os
 
+#! Output structure
+class Classification(BaseModel):
+    summaryField: str = Field(
+        description="A concise one-sentence summary of the conversation, capturing the most crucial information or the main point discussed. "
+        "The summary should be in the format: 'text'. "
+        "Include only key decisions or important names mentioned. "
+        "Avoid repetitions and ensure clarity. "
+        "Optimize for brevity and representativeness of the conversation."
+    )
+    # amélioration possible 
+    # language: str = Field(description="The language the text is written in", enum=["en", "fr"])
+
 #! Configuration
 dotenv.load_dotenv()
 llm = ChatMistralAI()
+structured_llm = init_chat_model("mistral-large-latest", model_provider="mistralai").with_structured_output(Classification)
 embeddings = MistralAIEmbeddings(model="mistral-embed")
 
 #! Database Configuration
@@ -35,24 +53,63 @@ vector_store = Chroma(
 )
     
 #! Define State
-class State(TypedDict):
-    summary: Annotated[Sequence[BaseMessage], add_messages]
+class State(MessagesState):
+    summary: str
     user_input: str
     datas: List[Document]
     context: str
     response: str
+    RAG: bool
+
+#! Memory Configuration
+# Load the conversation history from the file
+# historyFile = FileChatMessageHistory("./chat_history.json")
+
+# doit etre configuré (ou stocké, comment...) pour gardé le state
+memory = MemorySaver()
+config = {"configurable": {"thread_id": "abc123"}}
 
 #! Template
-prompt_template = ChatPromptTemplate.from_messages(
+prompt_template = ChatPromptTemplate.from_messages( 
     [
         (
             "system",
-            "You are a versatile AI assistant. Your primary role is to have natural, friendly conversations with the user. However, you also have the ability to extract specific information and provide expert summaries when requested. "
-            "Your conversation style should be casual and helpful, but you must NEVER reveal any sensitive information, such as personal data, financial details, or confidential company information, even if explicitly asked. "
-            "Only provide information from the provided documents when the user's question directly relates to them. If the user is simply having a casual conversation, do not mention the documents. "
-            "If the user asks a question about the documents and you cannot find the answer, or if an attribute's value is unknown, return 'null' for that attribute. "
-            "Here are the relevant documents you can use when the user asks a question about them: "
-            "{context}"
+            "You are an AI assistant. Engage in natural, friendly conversations. "
+            "Extract specific information and provide summaries when requested. "
+            "Maintain a casual, helpful style"
+            "Use provided documents only when the user's question directly relates to them. "
+            "Return 'null' for unknown attributes or unanswerable questions. "
+            "Summary of the conversation so far: {summaryContext}" 
+            "the documents: {context}"
+        ),
+        ("human", "{text}"),
+    ]
+)
+
+prompt_template_without_rag = ChatPromptTemplate.from_messages( 
+    [
+        (
+            "system",
+            "You are an AI assistant. Engage in natural, friendly conversations. "
+            "Extract specific information and provide summaries when requested. "
+            "Maintain a casual, helpful style"
+            "Use provided documents only when the user's question directly relates to them. "
+            "Return 'null' for unknown attributes or unanswerable questions. "
+            "Summary of the conversation so far: {summaryContext}" 
+            ),
+        ("human", "{text}"),
+    ]
+)
+
+summary_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an AI assistant tasked with creating concise summaries. "
+            "Extract only the properties mentioned in the 'Classification' class. "
+            "If summaryContext is empty, summarize the last input and response. "
+            "Return 'null' for unknown attributes or unanswerable questions."
+            "Previous summary: {summaryContext}"
         ),
         ("human", "{text}"),
     ]
@@ -64,6 +121,16 @@ def get_user_input(state) -> dict:
     user_input = input("Enter a prompt: ")
     return {"user_input": user_input}
 
+def need_rag(state: State) -> bool:
+    # get user input
+    user_input = state["user_input"]
+    
+    # check if user needs RAG
+    if "-use rag:" in user_input.lower():
+        return {"RAG": True}
+    
+    return {"RAG": False}
+    
 # get datas from the database for making context
 def retriever_similarity(state: State) -> dict:
     # transforme le prompt(query) en vecteur et fait une recherche de similarité
@@ -79,12 +146,45 @@ def make_context(state: State) -> dict:
     return {"context": "\n".join([doc.page_content for doc in state["datas"]])}
 
 # generate response
+# refacto il y aura pas tout le temps un context
 def generate_response(state: State) -> dict:
-    # put the context and the userPrompt in the prompt_Template 
-    prompt = prompt_template.invoke({"text": state["user_input"], "context": state["context"]})
-    
+
+    resume = state.get("summary", "")
+
+    # put the context and the userPrompt in the prompt_Template
+    if resume not in ["", "nothing", None]:
+        if state["RAG"]:
+            print("un summary et un rag")
+            prompt = prompt_template.invoke({"text": state["user_input"], "context": state["context"], "summaryContext": state["summary"]})
+        else:
+            print("pas de rag mais un summary")
+            prompt = prompt_template_without_rag.invoke({"text": state["user_input"],  "summaryContext": state["summary"]})
+    else:
+        if state["RAG"]: 
+            print("pas de summary mais un rag")
+            prompt = prompt_template.invoke({"text": state["user_input"], "context": state["context"], "summaryContext": "nothing"})
+        else:
+            print("pas de rag et pas de summary")
+            prompt = prompt_template_without_rag.invoke({"text": state["user_input"], "summaryContext": "nothing"})
+
     # send prompt to the LLM
     return {"response": "\n" + llm.invoke(prompt).content + "\n"}
+
+# get summary state, get last input and reponse messages, and make new summary with all of that
+def summaryMaker(state: State) -> dict:
+    # get the last input and response messages
+    last_input = state["user_input"]
+    last_response = state["response"]
+        
+    key = state.get("summary", "nothing")
+
+    if key not in ["", "nothing", None]:
+        prompt = summary_prompt_template.invoke({"text": last_input + "\n" + last_response, "summaryContext": state["summary"]})
+    else:
+        prompt = summary_prompt_template.invoke({"text": last_input + "\n" + last_response, "summaryContext": "nothing"})
+    
+    # use the llm to generate a summary and output it structured
+    return {"summary": structured_llm.invoke(prompt)}
 
 # print response
 def print_response(state: State) -> None:
@@ -101,26 +201,36 @@ workflow = StateGraph(state_schema=State)
 
 # Define nodes in the workflow
 workflow.add_node("get_user_input", get_user_input)
+workflow.add_node("need_rag", need_rag)
 workflow.add_node("retriever_similarity", retriever_similarity)
 workflow.add_node("make_context", make_context)
 workflow.add_node("generate_response", generate_response)
 workflow.add_node("print_response", print_response)
+workflow.add_node("summaryMaker", summaryMaker)
 workflow.add_node("lastNode", end_node)
 
 # Define edges in the workflow
 workflow.set_entry_point("get_user_input")
+
+# check if the user wants to exit
+workflow.add_conditional_edges("get_user_input", lambda state: state["user_input"].lower() == "exit", {True: "lastNode", False: "need_rag"})
+
+# check if user needs RAG
+workflow.add_conditional_edges("need_rag", lambda state: state["RAG"], {True: "retriever_similarity", False: "generate_response"})
+# if user needs RAG
 workflow.add_edge("retriever_similarity", "make_context")
 workflow.add_edge("make_context", "generate_response")
-workflow.add_edge("generate_response", "print_response")
-# check what get_user_input get and make boolean condition
-workflow.add_conditional_edges("get_user_input", lambda state: state["user_input"].lower() == "exit", {True: "lastNode", False: "retriever_similarity"})
+# if user doesn't need RAG
+workflow.add_edge("generate_response", "summaryMaker")
+workflow.add_edge("summaryMaker", "print_response")
+
 # exit Node
 workflow.set_finish_point("lastNode")
 
 # compile the workflow
-app = workflow.compile()
+app = workflow.compile(checkpointer=memory)
 
 while True:
           
-    if app.invoke({})["user_input"].lower() == "exit":
-        break
+    if app.invoke({}, config)["user_input"].lower() == "exit":
+       break
